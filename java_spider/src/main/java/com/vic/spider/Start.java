f46b4e715e71185f84a1596c7bb6e852cdd68597
vicoqi <Vicxiaoqi@outlook.com>
2018-05-14 18:40:39 +0800

    修复

--- java_spider/src/main/java/com/vic/spider/Start.java
+++ java_spider/src/main/java/com/vic/spider/Start.java
@@ -1,10 +1,27 @@
 package com.vic.spider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vic.spider.common.config.CommonConfig;
+import com.vic.spider.douban.DoubanHttpClient;
+import com.vic.spider.proxytool.ProxyHttpClient;
+
 public class Start {
-
+	private static Logger logger = LoggerFactory.getLogger(StartClass.class);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	        ProxyHttpClient.getInstance().startProxy();
+	        try {
+	            Thread.sleep(10000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	        if (CommonConfig.FUTURE_DOUBAN) {
+	            DoubanHttpClient.getInstance().startDouBan();
+	        }
 
-	}
-
+//	        if (CommonConfig.FUTURE_ZHIHU) {
+//	            ZhiHuHttpClient.getInstance().startZhiHu();
+//	        }
+	    }
 }
